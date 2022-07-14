02977ec27f494c6b3eaf5b2dbd06e3bf41fa7314
SirNaCl <emil.gedenryd@gmail.com>
2022-07-14 19:23:18 +0200

    a

--- linked.c
+++ linked.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 
 void *malloc(size_t size)
 {
-    write(STDOUT_FILENO, "Malloc", sizeof "Malloc");
-    return NULL;
+    return malloc(size);
 }
