b4e69e5556756e62637f78348f967ae7cfb883f7
Emil Gedenryd <emil.gedenryd@gmail.com>
2022-07-24 22:59:09 +0200

    realloc

--- linked.c
+++ linked.c
@@ -46,6 +46,7 @@ block_head_t *grow_heap(block_head_t *last_free, size_t size)
 
 block_head_t *claim_and_split(block_head_t *block, size_t size)
 {
+    block->free = 0;
     if (block->size - HEADSIZE - size >= MINSIZE)
     {
         // Split excess into new block if worthwhile
@@ -79,6 +80,21 @@ block_head_t *find_free(size_t size)
     return grow_heap(block, size);
 }
 
+void consolidate_blocks()
+{
+    block_head_t *block = root;
+    while (block && block->next)
+    {
+        if (block->free && block->next->free)
+        {
+            block->size += block->next->size + HEADSIZE;
+            block->next = block->next->next;
+            continue; // If we consolidated, we don't have to jump to next block
+        }
+
+        block = block->next;
+    }
+}
 // Allocate a block of size "size" to the heap
 void *malloc(size_t size)
 {
@@ -104,22 +120,6 @@ void *malloc(size_t size)
     return NULL;
 }
 
-void consolidate_blocks()
-{
-    block_head_t *block = root;
-    while (block && block->next)
-    {
-        if (block->free && block->next->free)
-        {
-            block->size += block->next->size + HEADSIZE;
-            block->next = block->next->next;
-            continue; // If we consolidated, we don't have to jump to next block
-        }
-
-        block = block->next;
-    }
-}
-
 void free(void *ptr)
 {
     if (!ptr)
@@ -128,4 +128,33 @@ void free(void *ptr)
     assert(block->free == 0);
     block->free = 1;
     consolidate_blocks();
+}
+
+void *realloc(void *ptr, size_t size)
+{
+    // Return null if invalid pionter or size
+    if (!ptr || size <= 0)
+        return NULL;
+
+    block_head_t *block = ((block_head_t *)ptr) - 1;
+    assert(block->free == 0);
+
+    // No changes if size is the same
+    if (size == block->size)
+        return (block + 1);
+
+    // Reduce size
+    if (size < block->size)
+        return (claim_and_split(block, size) + 1);
+
+    // Find new block if increasing size
+    // TODO: Check if block can expand
+    block_head_t *nb = find_free(size);
+    if (!nb)
+        return NULL;
+
+    // Move content to new block
+    memcpy((nb + 1), ptr, block->size);
+    free(ptr);
+    return (nb + 1);
 }
