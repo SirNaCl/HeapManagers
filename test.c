2bdb960407a7ea703fd97b1ffec62ff4d29144fc
Emil Gedenryd <emil.gedenryd@gmail.com>
2022-07-24 18:44:59 +0200

    a

--- test.c
+++ test.c
@@ -9,16 +9,20 @@ int main()
     char *str;
 
     /* Initial memory allocation */
+    printf("Running malloc tests");
     str = (char *)malloc(13);
     strcpy(str, "mallocstring");
     assert(strcmp(str, "mallocstring") == 0);
-
+    printf("Malloc tests PASSED \n");
     /* Reallocating memory */
+
+    printf("Running realloc tests");
     str = (char *)realloc(str, 16);
     strcat(str, "new");
     assert(strcmp(str, "mallocstringnew") == 0);
+    printf("Realloc tests PASSED \n");
 
     free(str);
-    printf("Tests passed\n");
+    printf("All tests passed\n");
     return (0);
 }
