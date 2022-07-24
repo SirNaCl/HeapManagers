efaf3b3e22935bf5c76d020fd6efc0f7594615c0
Emil Gedenryd <emil.gedenryd@gmail.com>
2022-07-24 23:13:43 +0200

    a

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
-#include "linked.c"
+//#include "linked.c"
 
 int main()
 {
@@ -21,8 +21,22 @@ int main()
     strcat(str, "new");
     assert(strcmp(str, "mallocstringnew") == 0);
     printf("Realloc tests PASSED \n");
-
     free(str);
+
+    printf("Running calloc tests\n");
+    int *a;
+
+    a = (int *)calloc(3, sizeof(int));
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 2;
+
+    assert(a[0] == 0);
+    assert(a[1] == 1);
+    assert(a[2] == 2);
+    free(a);
+    printf("Calloc tests PASSED \n");
+
     printf("All tests passed\n");
     return (0);
 }
