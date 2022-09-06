37372178d64e012d4473914b6f0924ac0f5d340a
Emil Gedenryd <emil.gedenryd@gmail.com>
2022-09-06 23:29:48 +0200

    asdf

--- buddy.c
+++ buddy.c
@@ -29,7 +29,8 @@ head_t *new_block()
     long int adr = (long int)sbrk(BLOCKSIZE << 1); // Allocate largest block plus alignment buffer
     // long int mask = 0xfffff<< LEVELS + MINEXP - 1;
     // long int mask = ~((0x1 << (LEVELS + MINEXP)) - 1);
-    long int mask = 0xfffff << (LEVELS + MINEXP - 1);
+    // long int mask = 0xfffff << (LEVELS + MINEXP - 1);
+    long int mask = ~0 - ((1 << (LEVELS + MINEXP)) - 1);
     adr += aligned;
     adr &= mask;
 
